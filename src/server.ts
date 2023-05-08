7327945d60aa250c1576afcef63011ad201e74f1
jlts <jlts@gft.com>
2023-05-08 15:58:49 -0300

    https

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,11 @@ import { AppError } from './error/AppError';
 import fs from 'fs';
 import https from 'https';
 
+const file = fs.readFileSync('./3969C06766DE24A8843D74E38B842C9B.txt')
 const key = fs.readFileSync('private.key')
 const cert = fs.readFileSync('certificate.crt')
 const app = express()
+const port = 3000
 
 const cred = {
     key,
@@ -34,8 +36,10 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
         message: `Internal server error -  ${err.message}`
     })
 })
-
-app.listen(3000, () => console.log('API downloads rodando na porta 3000...🚀'));
+app.get('/.well-known/pki-validation/3969C06766DE24A8843D74E38B842C9B.txt', (req: Request, res: Response) => {
+    res.sendFile('/home/ubuntu/API/3969C06766DE24A8843D74E38B842C9B.txt')
+})
+app.listen(port, () => console.log('API downloads rodando na porta 3000...🚀'));
 
 const httpsServer = https.createServer(cred, app)
-httpsServer.listen(3000)
+httpsServer.listen(8443)
